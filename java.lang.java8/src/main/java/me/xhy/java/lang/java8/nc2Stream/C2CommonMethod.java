722984a804e2181218acac733b0a0d3ce7e42ef9
wowxhycoming <wowxhycoming@sina.com>
2017-02-09 15:02:04 +0800

    增加原材料，增加Stream

---
+++ java.lang.java8/src/main/java/me/xhy/java/lang/java8/nc2Stream/C2CommonMethod.java
@@ -0,0 +1,7 @@
+package me.xhy.java.lang.java8.nc2Stream;
+
+/**
+ * Created by xuhuaiyu on 2017/2/8.
+ */
+public class C2CommonMethod {
+}
---
+++ java.lang.materials/src/main/java/me/xhy/java/lang/materials/music/Music.java
@@ -0,0 +1,20 @@
+package me.xhy.java.lang.materials.music;
+
+import java.util.stream.Stream;
+
+import static java.util.stream.Stream.concat;
+
+public interface Music {
+
+    public String getName();
+
+    public Stream<Artist> getMusicians();
+
+    // TODO: test
+    public default Stream<Artist> getAllMusicians() {
+        return getMusicians().flatMap(artist -> {
+            return concat(Stream.of(artist), artist.getMembers());
+        });
+    }
+
+}
