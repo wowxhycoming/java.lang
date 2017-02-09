6651da3baaaaee63b6dcafb3463ac6a266e28f47
wowxhycoming <wowxhycoming@sina.com>
2017-02-09 14:38:33 +0800

    space replace table

--- java.lang.java8/src/main/java/me/xhy/java/lang/java8/nc1Lambda/C3ImportantApi.java
+++ java.lang.java8/src/main/java/me/xhy/java/lang/java8/nc1Lambda/C3ImportantApi.java
@@ -6,17 +6,17 @@ import java.util.function.*;
  * Created by xuhuaiyu on 2017/2/7.
  */
 public class C3ImportantApi {
-	/**
-	 * Predicate<T> T boolean
-	 * Consumer<T> T void
-	 * Function<T, R> T R
-	 * Supplier<T> None T
-	 * UnaryOperator<T> T T
-	 * BinaryOperator<T,T> (T,T) T
-	 */
+    /**
+     * Predicate<T> T boolean
+     * Consumer<T> T void
+     * Function<T, R> T R
+     * Supplier<T> None T
+     * UnaryOperator<T> T T
+     * BinaryOperator<T,T> (T,T) T
+     */
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-	}
+    }
 
 }
