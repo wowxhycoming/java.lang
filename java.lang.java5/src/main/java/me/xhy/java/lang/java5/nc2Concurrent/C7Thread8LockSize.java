ecc04a741c93309e8e376b0dc94d3a20540355f4
xuhuaiyu <wowxhycoming@sina.com>
2017-04-03 00:10:43 +0800

    * 完成线程锁的8种情况的例子

--- java.lang.java5/src/main/java/me/xhy/java/lang/java5/nc2Concurrent/C7Thread8LockSize.java
+++ java.lang.java5/src/main/java/me/xhy/java/lang/java5/nc2Concurrent/C7Thread8LockSize.java
@@ -12,59 +12,344 @@ package me.xhy.java.lang.java5.nc2Concurrent;
  * 7. 一个静态同步方法，一个非静态同步方法，两个 Number 对象?  //two  one
  * 8. 两个静态同步方法，两个 Number 对象?   //one  two
  *
- * 线程八锁的关键：
- * ①非静态方法的锁默认为 this,  静态方法的锁为 对应的 Class 实例
- * ②某一个时刻内，只能有一个线程持有锁，无论几个方法。
+ * 线程锁是否互斥的关键：
+ * 1. 非静态方法的锁默认为 this,  静态方法的锁为 对应的 Class 实例
+ * 2. 某一个时刻内，只能有一个线程持有锁，无论几个方法。
  */
 public class C7Thread8LockSize {
 
     public static void main(String[] args) {
-        Number number = new Number();
-        Number number2 = new Number();
+
+        // 两个普通方法无锁
+//        s1();
+
+        // 非静态方法上的 synchronized 锁的是 <this> ， 只 new 一个对象的情况下，锁是相同的，要排队。
+//        s2();
+
+        // 普通方法与锁无关
+//        s3();
+
+        // 非静态方法上的 synchronized 在多对象的情况下互不干预，因为锁的是 this，多个对象，多个this，多个锁。
+//        s4();
+
+        // 静态方法上的锁是 Object.class ， 相同对象下的两个方法锁不同，一个是Object.class，一个是this
+//        s5();
+
+        // 两个方法都是静态方法，锁相同，排队
+//        s6();
+
+        //
+//        s7();
+
+        // 两个对象的方法中，锁都是 Object.class
+//        s8();
+
+
+
+    }
+
+
+    // 同一对象上的 不同 synchronized 方法
+    private static void s1() {
+
+        Number1 number1 = new Number1();
 
         new Thread(new Runnable() {
             @Override
             public void run() {
-                number.getOne();
+                number1.getOne();
             }
         }).start();
 
         new Thread(new Runnable() {
             @Override
             public void run() {
-//              number.getTwo();
+                number1.getTwo();
+            }
+        }).start();
+    }
+
+    private static void s2() {
+
+        Number2 number2 = new Number2();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                number2.getOne();
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
                 number2.getTwo();
             }
         }).start();
+    }
 
-        /*new Thread(new Runnable() {
+    private static void s3() {
+
+        Number3 number3 = new Number3();
+
+        new Thread(new Runnable() {
             @Override
             public void run() {
-                number.getThree();
+                number3.getOne();
             }
-        }).start();*/
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                number3.getTwo();
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                number3.getThree();
+            }
+        }).start();
+    }
+
+    private static void s4() {
+
+        Number4 number4_1 = new Number4();
+        Number4 number4_2 = new Number4();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                number4_1.getOne();
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                number4_2.getTwo();
+            }
+        }).start();
+
+    }
+
+    private static void s5() {
+
+        Number5 number5 = new Number5();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                number5.getOne();
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                number5.getTwo();
+            }
+        }).start();
+
+    }
+
+    private static void s6() {
+
+        Number6 number6 = new Number6();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                number6.getOne();
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                number6.getTwo();
+            }
+        }).start();
+
+    }
+
+    private static void s7() {
+
+        Number7 number7_1 = new Number7();
+        Number7 number7_2 = new Number7();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                number7_1.getOne();
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                number7_2.getTwo();
+            }
+        }).start();
+
+    }
+
+    private static void s8() {
+
+        Number8 number8_1 = new Number8();
+        Number8 number8_2 = new Number8();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                number8_1.getOne();
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                number8_2.getTwo();
+            }
+        }).start();
 
     }
 
 }
 
-class Number {
+class Number1 {
 
-    public static synchronized void getOne() {//Number.class
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-        }
-
+    public synchronized void getOne() {
         System.out.println("one");
     }
 
-    public synchronized void getTwo() {//this
+    public synchronized void getTwo() {
+        System.out.println("two");
+    }
+
+}
+
+class Number2 {
+
+    public synchronized void getOne() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("one");
+    }
+
+    public synchronized void getTwo() {
+        System.out.println("two");
+    }
+
+}
+
+class Number3 {
+
+    public synchronized void getOne() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("one");
+    }
+
+    public synchronized void getTwo() {
         System.out.println("two");
     }
 
     public void getThree() {
         System.out.println("three");
+    }
+
+}
+
+class Number4 {
+
+    public synchronized void getOne() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("one");
+    }
+
+    public synchronized void getTwo() {
+        System.out.println("two");
+    }
+
+}
+
+class Number5 {
+
+    public static synchronized void getOne() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("one");
+    }
+
+    public synchronized void getTwo() {
+        System.out.println("two");
+    }
+
+}
+
+class Number6 {
+
+    public static synchronized void getOne() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("one");
+    }
+
+    public static synchronized void getTwo() {
+        System.out.println("two");
+    }
+
+}
+
+class Number7 {
+
+    public static synchronized void getOne() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("one");
+    }
+
+    public synchronized void getTwo() {
+        System.out.println("two");
+    }
+
+}
+
+class Number8 {
+
+    public static synchronized void getOne() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("one");
+    }
+
+    public static synchronized void getTwo() {
+        System.out.println("two");
     }
 
 }
